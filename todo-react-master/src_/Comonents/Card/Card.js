ea7381e133eea4df07bbdc5fd8850b50eca1b2c1
Dreptus <dreptus@gmail.com>
2022-07-05 21:00:45 +0200

    task 17.6

--- todo-react-master/src_/Comonents/Card/Card.js
+++
@@ -1,7 +0,0 @@
-import classes from "./Card.module.scss";
-
-const Card = (props) => {
-  return <li className={classes.card}>{props.title}</li>;
-};
-
-export default Card;
--- todo-react-master/src_/Comonents/Container/Container.js
+++
@@ -1,5 +0,0 @@
-const Container = (props) => {
-  return <div>{props.children}</div>;
-};
-
-export default Container
